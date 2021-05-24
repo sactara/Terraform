c6e4efd7f5e0c0ee0d36ab14b81d461e9d131b92
sactara <37993950+sactara@users.noreply.github.com>
2021-05-24 22:33:31 +0530

    Update Create_EC2.tf

--- Create_EC2.tf
+++ Create_EC2.tf
@@ -5,6 +5,10 @@ resource "aws_instance" "terraform_ec2" {
  ami = "ami-0b0af3577fe5e3532"
  instance_type = "t2.micro"
  tags = {
-  Name = "Terraform EC2"
+  Name = "Terraform_EC2"
  }
+}
+provider "github" {
+  token        = "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"
+  organization = "Terraform_EC2"
 }
